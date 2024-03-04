b26797432bc52c02b71e14ab4ec5849708edbcbd
jwburke256 <jwburke256@gmail.com>
2024-03-04 15:05:51 -0800

    adjusted run_script

--- run_script_linux.sh
+++ run_script_linux.sh
@@ -4,15 +4,17 @@
 set -e
 
 # run webscraper and setup SQL database
-python3 jwstDataFinder.py
+cd ./WebScraping
+python3 ./jwstDataFinder.py
 process_id=$!
 wait $process_id
 echo "jwstDataFinder.py executed"
-python3 jwstJson.py
-process_id=$!
-wait $process_id
-echo "jwstJson.py executed"
-python3 jwstDatabase.py
-process_id=$!
-wait $process_id
-echo "jwstDatabase.py executed"
+#python3 jwstJson.py
+#process_id=$!
+#wait $process_id
+#echo "jwstJson.py executed"
+#python3 jwstDatabase.py
+#process_id=$!
+#wait $process_id
+#echo "jwstDatabase.py executed"
+echo "run_script execution complete!"
