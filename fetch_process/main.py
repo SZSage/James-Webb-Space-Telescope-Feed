982136287367097781fe8517b43a6b2a0424a72a
SZSage <simonz@uoregon.edu>
2024-03-05 13:19:51 -0800

    Made some adjustments to include automated fetching to main logic

--- fetch_process/main.py
+++ fetch_process/main.py
@@ -2,7 +2,7 @@
 Filename: main.py
 Author: Simon Zhao
 Date Created: 03/02/2024
-Date Last Modified: 03/02/2024
+Date Last Modified: 03/05/2024
 Description: This is the main script that utilizes the MastQuery and Processing class.
 """
 
@@ -12,16 +12,24 @@ from convert import Processing
 def main() -> None:
     # mast authorization
     mast_token = ""
-    query = MastQuery("M82", "NIRCam", "downloaded_fits/")
+    query = MastQuery("MCS-J1149.5+2223", "NIRCam", "downloaded_fits/")
     query.mast_auth(mast_token)
+    db_path = "../WebScraping/jwstDatabaseFile.sqlite"
+    #query.query_masts()
+    list_of_weekly_dataframes = query.fetch_and_segment_by_week(db_path)
+    weekly_count = 0
+    for weekly_dataframe in list_of_weekly_dataframes:
+        weekly_count += 1
+        print(f"Processing week: {weekly_count} with {len(weekly_dataframe)} observation")
+        query.process_all_rows_from_db(weekly_dataframe, weekly_count)
 
-    # aquire fits data
-    fits_data = query.fetch_fits_data()
-    new_url = query.combine("jw01701052001_02106_00016_nrcblong_i2d.fits")
+    #fit_process = Processing()
+    #new_url = query.combine("jw02883-o008_s00256_nircam_f480m-grismc_x1d.fits")
+    """
+
     hdul_data = query.stream_fits_data(new_url)
 
     # process fits data 
-    fit_process = Processing()
     #processed = fit_process.process_fits(new_url, use_asinh=False)
     #fit_process.inspect_fits_content(new_url)
     
@@ -36,8 +44,9 @@ def main() -> None:
     #fit_process.visualize_fits(image_sqrt)
     #fit_process.create_pseudo_rgb_image(new_url, "pseudo_rgb.png")
     #fit_process.convert_to_png(processed, "hello.png", True)
-    fit_process.compare_scaling_methods(new_url)
+    """
 
+    #fit_process.compare_scaling_methods(new_url)
 
 if __name__ == "__main__":
     main()
