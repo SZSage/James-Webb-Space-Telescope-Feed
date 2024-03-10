6d08c45bcc0fcff5ea1a97ec1abfddb0c6f8f82d
b3lla-c0rtez <103864274+b3lla-c0rtez@users.noreply.github.com>
2024-03-10 13:08:15 -0700

    fixing comments for web scraping files

--- WebScraping/jwstDatabase.py
+++ WebScraping/jwstDatabase.py
@@ -3,30 +3,28 @@ jwstDatabase.py: CS 422 Project 2
 Author: Isabella Cortez
 Credit: easyA github of willard's database implementation
 This file takes the data from the jwst_data.json file and converts it to sqlite format
-Date Modified: 2/27/2024
+Date Modified: 3/10/2024
 """
 
 import sqlite3
 import json
 
-
+# open json file
 with open('jwst_data.json') as file:
     jwst_data_json = file.read()
 
-
-# jwst_data = json.loads(jwst_data_json)
-#jwst_data_json = jwst_data_json.split('[')[-1].split(']')[0]
-
+# handle error exceptions
 try:
     jwst_data = json.loads(jwst_data_json)
 except json.JSONDecodeError as e:
     print(f"Error decoding JSON: {e}")
     exit()
 
-
+# make connection to database
 conn = sqlite3.connect('jwstDatabaseFile.sqlite')
 cursor = conn.cursor()
 
+# create table with these heading names
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS jwst_data (
         visit_id TEXT,
@@ -41,9 +39,9 @@ cursor.execute('''
     )
 ''')
 
-
+# put data in these headings
 for data_point in jwst_data:
-   cursor.execute("INSERT INTO jwst_data VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+    cursor.execute("INSERT INTO jwst_data VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
                    (data_point.get('VISIT ID', ''),
                     data_point.get('PCS MODE', ''),
                     data_point.get('VISIT TYPE', ''),
@@ -54,38 +52,9 @@ for data_point in jwst_data:
                     data_point.get('CATEGORY', ''),
                     data_point.get('KEYWORDS', '')))
 
-
-
-# jwst_data_json[5:]
-'''
-# for line in jwst_data_json[5:]:  # Skip the header row
-try:
-        # Split the line into individual JSON objects and parse each one
-        # row_data = json.loads(line)
-    data_list = json.loads("[" + jwst_data_json + "]")
-    # row_data = json.loads(line.strip().rstrip(','))
-    for row_data in data_list:
-
-        row = (
-            row_data.get('visit_id'),
-            row_data.get('pcs_mode'),
-            row_data.get('visit_type'),
-            row_data.get('scheduled_start_time'),
-            row_data.get('duration'),
-            row_data.get('science_instrument_and_mode'),
-            row_data.get('target_name'),
-            row_data.get('category'),
-            row_data.get('keywords')
-        )
-    conn.execute("INSERT INTO jwst_data VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", row)
-
-except json.JSONDecodeError as e:
-    print(f"Error decoding JSON on line: {jwst_data_json}")
-    print(f"Error details: {e}")
-    # continue  # Skip to the next line
-'''
 # Commit changes and close the connection
 conn.commit()
 conn.close()
 
+# print to know it is done
 print("Data inserted successfully into the SQLite database.")
--- WebScraping/jwstJson.py
+++ WebScraping/jwstJson.py
@@ -3,12 +3,14 @@ jwstJson.py: CS 422 Project 2
 Author: Isabella Cortez
 Credit:
 This file takes the data from the jwst_data.txt file and converts it to json format (output is jwst_data.json)
-Date Modified: 2/27/2024
+Date Modified: 3/10/2024
 """
 
 import re
 import json
 
+
+# convert txt to json
 def parse_txt_to_json(txt_file):
     # Read the content of the text file
     with open(txt_file, 'r') as file:
@@ -33,31 +35,23 @@ def parse_txt_to_json(txt_file):
 
     # Iterate over lines below the header to extract visit information
     for line in lines[header_line + 2:]:
-        # visit_info = re.findall(r'"([^"]*)"|(\S+)', line)
-        # visit_info = [value[0] if value[0] else value[1] for value in visit_info]
-        # visit_info = [" ".join(value) if value[0] else value[1] for value in visit_info]
-        # visit_info = [value.strip('"') if '"' in value else value for value in re.findall(r'"([^"]*)"|(\S+)', line)]
         visit_info = line.split('  ')
         visit_info = [value.strip() for value in visit_info if value.strip()]
         # print(visit_info)
 
-        # Check if the line is not empty
-        # if visit_info:
-            # visit_string = ""
-            # for item in visit_info:
-               # visit_string += item
-            # print(visit_string)
-            # Combine the headers and visit information into a dictionary
-
+        # iterate through headers and visit_info list
         visit_dict = dict(zip(headers, visit_info))
 
-
+        # append them to visit_info_list
         visit_info_list.append(visit_dict)
 
-
+    # return list
     return visit_info_list
 
+# text file to read from
 txt_file = "jwst_data.txt"
+
+# what to call json file
 json_output_file = "jwst_data.json"
 
 # Parse the text file and get visit information list
@@ -67,7 +61,5 @@ visit_info_list = parse_txt_to_json(txt_file)
 with open(json_output_file, 'w') as json_file:
     json.dump(visit_info_list, json_file, indent=2)
 
-# for visit_dict in visit_info_list:
-    # print(" ".join(f"{key}: {value}" for key, value in visit_dict.items()))
-
+# print to make sure info is in json file
 print(f"JSON data has been written to {json_output_file}.")
