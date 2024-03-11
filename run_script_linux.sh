5a09600fde34c0980a52952363fd7c620118d03c
jwburke256 <jwburke256@gmail.com>
2024-03-11 12:05:39 -0700

    continuos work on run_script

--- run_script_linux.sh
+++ run_script_linux.sh
@@ -4,17 +4,31 @@
 set -e
 
 # run webscraper and setup SQL database
-cd ./WebScraping
+cd ./tests
+pytest mast_query_test.py
+process_id=$!
+wait $process_id
+echo "tests executed"
+cd ../WebScraping
 python3 ./jwstDataFinder.py
 process_id=$!
 wait $process_id
 echo "jwstDataFinder.py executed"
-#python3 jwstJson.py
-#process_id=$!
-#wait $process_id
-#echo "jwstJson.py executed"
-#python3 jwstDatabase.py
-#process_id=$!
-#wait $process_id
-#echo "jwstDatabase.py executed"
-echo "run_script execution complete!"
+python3 jwstJson.py
+process_id=$!
+wait $process_id
+echo "jwstJson.py executed"
+python3 jwstDatabase.py
+process_id=$!
+wait $process_id
+echo "jwstDatabase.py executed"
+cd ../fetch_process
+python3 ./main.py
+process_id=$!
+wait $process_id
+echo "fetch process main.py executed"
+#cd ../nebulanet
+#npm install -g serve
+#serve -s build
+#cd ..
+#echo "run_script execution complete!"
