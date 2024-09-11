3b311dbda400c5c2f3b5f2e3f7b9a563aa0eda53
b3lla-c0rtez <103864274+b3lla-c0rtez@users.noreply.github.com>
2024-09-10 23:00:36 -0700

    Update Wecome.js

--- nebulanet/src/components/Wecome.js
+++ nebulanet/src/components/Wecome.js
@@ -8,7 +8,7 @@ function Welcome () {
                 <div className="welcome">
                     NebulaNet aims to be a resource displaying the daily observations
                     taken by the James Webb Telescope. Contrary to popular belief, the
-                    photos that many of us have seen are composity images made up of hundreds
+                    photos that many of us have seen are composite images made up of hundreds
                     of different individual photos using a variety of different of instruments
                     and image settings. These are then converted into png photos using different
                     algorithms depending on the tyoe of subject the image was focusing on.
